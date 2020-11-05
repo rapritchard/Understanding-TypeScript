24edb64a4032b0eb9a4af19a3bb0625c1693bb35
Reece Pritchard <32645551+rapritchard@users.noreply.github.com>
2020-11-05 16:17:07 +0000

    Basics + objs-arrays-enums

--- objs-arrays-enums.ts
+++ objs-arrays-enums.ts
@@ -13,26 +13,25 @@
 //     hobbies: ['Sports', 'Cooking'],
 //     role: [2, 'author']
 // };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
     name: 'Reece',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
     role: Role.ADMIN
 };
+
 // person.role.push('admin');
 // person.role[1] = 10;
+
 console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
+
+for (const hobby of person.hobbies) {
     console.log(hobby);
 }
+
 if (person.role === Role.AUTHOR) {
     console.log('is author');
 }
