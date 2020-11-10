8a332def45db3f975f0113ab049b831e873a2e96
Reece Pritchard <32645551+rapritchard@users.noreply.github.com>
2020-11-10 00:14:47 +0000

    unknown + never types

---
+++ app.ts
@@ -0,0 +1,14 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+
+function generatorError(message: string, code: number): never {
+    throw { message: message, errorCode: code};
+}
+
+generatorError('An error occurred!', 500);
