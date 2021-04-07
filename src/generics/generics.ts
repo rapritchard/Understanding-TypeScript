5146adcc88d267b41992b46db54b59dae26a0a57
Reece Pritchard <rapritchard96@gmail.com>
2021-04-07 16:05:35 +0100

    Learnt about generic types.

---
+++ src/generics/generics.ts
@@ -0,0 +1,96 @@
+// const names: Array<string> = []; // string[]
+// // names[0].split(" ");
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then(data => {
+//   // data.split(" ");
+// });
+
+// Generic Function with constraint
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 25 });
+console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1 ) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({name: "Max"}, "name"));
+// Generic Function with constraint End
+// Generic Classes
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({name: "Max"});
+// objStorage.addItem({name: "Manu"});
+// objStorage.removeItem({name: "Max"});
+// console.log(objStorage.getItems());
+// Generic Classes End
+
+// Generic utility Types
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Anna"];
+// names.push("Manu");
+// Generic utility types End
