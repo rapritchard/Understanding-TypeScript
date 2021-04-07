27b6be10bb86174b1ea8cbe57686d7780ed7b5f0
Reece Pritchard <rapritchard96@gmail.com>
2021-04-07 14:06:01 +0100

    Learned advanced types.
    -Intersection types
    - Other type guards
    - Discriminated unions
    - Type casting
    - Index properties
    - Functional Overloads
    - Optional Chaining
    - Nullish coalescing

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,2 @@
 "use strict";
-let add;
-add = (n1, n2) => {
-    return n1 + n2;
-};
-class Person {
-    constructor(name) {
-        this.age = 25;
-        if (name) {
-            this.name = name;
-        }
-    }
-    greet(phrase) {
-        if (this.name) {
-            console.log(phrase + " " + this.name);
-        }
-        console.log("Hi!");
-    }
-}
-let user1;
-user1 = new Person();
-user1.greet("Hi there - I am");
-console.log(user1);
 //# sourceMappingURL=app.js.map
